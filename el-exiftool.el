5675d808dcec9598c74533ec114484cfda33f8bb
Arun Isaac <arunisaac@systemreboot.net>
2017-02-21 19:04:12 +0530

    Remove `subr' dependency.
    * el-exiftool.el (subr): Do not require `subr'.

--- el-exiftool.el
+++ el-exiftool.el
@@ -15,7 +15,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with el-exiftool.  If not, see <http://www.gnu.org/licenses/>.
 
-(require 'subr)
 (require 'subr-x)
 (require 'cl)
 
